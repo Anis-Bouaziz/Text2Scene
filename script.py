d37140e7d6bc6b9d37987f0ce3f8f7d1a58364ba
mohamed Anis Bouaziz <mohamedanis.bouaziz@esprit.tn>
2020-09-15 13:54:58 +0200

    Git lfs for large model

--- script.py
+++
@@ -1,7 +0,0 @@
-import patoolib
-import os
-patoolib.extract_archive("models/en/pretrain/ewt.rar", outdir="models/en/pretrain/")
-if os.path.exists("models/en/pretrain/ewt.rar"):
-  os.remove("models/en/pretrain/ewt.rar")
-else:
-  print("The file does not exist")
